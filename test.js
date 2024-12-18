b4c433d4d0430f0c2a4c56ecb4a57fb801e2f285
Rozario-joel <joeyrozario69@gmail.com>
2024-12-18 16:53:12 +0530

    test commit

---
+++ test.js
@@ -0,0 +1,14 @@
+
+const testnum = 50
+const testSting = `this is to learn git and git hub`
+
+const funt = () => {
+    console.log(testSting);
+    console.log(testnum);
+    
+}
+funt()
+
+console.log('making changes');
+
+
